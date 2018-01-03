69a4793a15df659c51cfc556cc3d465cdf3bb146
Jorge <jacorderox@gmail.com>
2018-01-02 23:13:19 -0600

    small fix to get the right count of counties

--- public/python_scripts/script_count_counties.py
+++ public/python_scripts/script_count_counties.py
@@ -15,5 +15,15 @@ if __name__ == '__main__':
     count = dataframe['County Code'].nunique()
     counties_freqs = dataframe['County Code'].value_counts()
     print("Number of unique counties: {}".format(count))
-    print("Counties freqs: {}".format(counties_freqs))
-
+    #print("Counties freqs: {}".format(counties_freqs))
+    grouped = dataframe.groupby("State")['County Code'].value_counts()
+    print("Unique counties by state\n{}".format(grouped))
+    #gframe = grouped.to_frame()
+    #gframe = gframe.reset_index(level=['State', 'County Code'])
+    #print(gframe)
+#    for group in grouped.groups:
+#        dgroup = grouped.groups[group]
+#        dgroup_count = dgroup['County Code'].nunique()
+#        print("{} has {} unique counties".format(group, dgroup_count))
+    
+    #print("States:\n{}".format(dataframe['State'].value_counts()))
